e6353b61ba64515156159179b82b252fa4cdaef6
febilly <fake@fake.com>
2024-11-04 20:30:19 +0800

    改善按钮名字的显示效果

--- Scripts/responsive_label.gd
+++ Scripts/responsive_label.gd
@@ -1,7 +1,13 @@
 extends Label
 class_name ResponsiveLabel
 
-@export var label_size_ratio: float = 0.8
+enum LabelSizeMode {
+	FIT,
+	COVER
+}
+
+@export_range(0.01, 1.0) var label_size_ratio: float = 0.8
+@export var size_mode: LabelSizeMode = LabelSizeMode.FIT
 
 @onready var original_size := size
 @onready var original_font_size := label_settings.font_size
@@ -9,6 +15,12 @@ class_name ResponsiveLabel
 
 func update_font_size(container_size: Vector2) -> void:
 	var factors: Vector2 = container_size / original_size
-	var scale = label_size_ratio * min(factors.x, factors.y)
-	label_settings.font_size = original_font_size * scale
-	label_settings.outline_size = original_outline_size * scale
+
+	var scale_ratio
+	if size_mode == LabelSizeMode.FIT:
+		scale_ratio = label_size_ratio * min(factors.x, factors.y)
+	else:
+		scale_ratio = label_size_ratio * max(factors.x, factors.y)
+	
+	label_settings.font_size = original_font_size * scale_ratio
+	label_settings.outline_size = original_outline_size * scale_ratio
